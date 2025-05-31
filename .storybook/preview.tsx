5e61257bcceda203040fcaade823912aaf98070a
Rodrigo Frenk <furenku@gmail.com>
2025-05-31 04:45:07 -0600

    fix base layout for stories

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import type { Preview } from '@storybook/react-native-web-vite'
 
 import { ThemeProvider } from "../context/ThemeContext"
+import { View } from 'react-native';
 
 const preview: Preview = {
   decorators: [
     (Story) => (
       <ThemeProvider>
-        <Story />
+        <View style={{ width: 375, height: 812,  backgroundColor: '#000',  display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Story />
+        </View>
       </ThemeProvider>
     ),
   ],
   parameters: {
+    layout: 'centered',
     controls: {
       matchers: {
        color: /(background|color)$/i,
